e15092f37c28ae220f7afa3fb470c3f4f9c3a525
YenXXXW <YenXXXW@gmai.com>
2022-08-19 09:37:25 +0630

    removes existing inventory

--- components/indexComp.js
+++ components/indexComp.js
@@ -64,16 +64,7 @@ function IndexComp({photo , model , order }) {
             <p className={model === 'Solar Panels'? 'text-center text-lg' : 'hidden' }>Lowest cost Solar Panels in America</p>
             <p className={model === 'Solar Roof'?  'text-center  text-lg' : 'hidden' }>Produce Clean Energy From Your Roof</p>
         </motion.div>
-        <div className="flex flex-col   sm:flex-row sm:justify-between absolute bottom-[80px] sm:w-[650px] left-0 right-0 ml-auto mr-auto ">
-            <motion.div className="cursor-pointer bg-gray-600 py-[9px] w-5/6 left-0 right-0 ml-auto mr-auto  sm:w-2/5 rounded-full text-white text-center"
-            variants={LeftVariants} initial='hidden' animate={controls}>
-                Custom Order
-            </motion.div>
-            <motion.div className="cursor-pointer w-5/6 left-0 right-0 ml-auto mr-auto mt-3 sm:mt-0 bg-gray-100 py-[9px] sm:w-2/5 rounded-full  text-center"
-            variants={RightVariants} initial='hidden' animate={controls}>
-                Existing Inventory
-            </motion.div>
-        </div>
+        
         </div>
         
         </>

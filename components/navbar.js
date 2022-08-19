8379b667633cd9de4c69a41791848d5b611d397d
YenXXXW <YenXXXW@gmai.com>
2022-08-19 11:03:13 +0630

    added width color

--- components/navbar.js
+++ components/navbar.js
@@ -51,7 +51,7 @@ const Navbar = ({fill , position }) => {
                 <p>Account</p>
                 <p onClick={()=>setMenu(true)}>Menu</p>
             </div>
-            <div className='mr-2 xl:hidden' onClick={()=>setMenu(true)}>
+            <div className='mr-2 xl:hidden bg-red-200 w-[200px]' onClick={()=>setMenu(true)}>
                 <button className='px-4 py-2 bg-[#e0e2e7] rounded-xl 
                 hover:bg-[#d3d4d8] font-bold'>Menu</button>
             </div>

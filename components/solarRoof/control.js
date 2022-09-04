722fb58bcaaa2cdd78b80cef02adee332b874ff7
YenXXXW <YenXXXW@gmai.com>
2022-09-04 17:08:42 +0630

    check

--- components/solarRoof/control.js
+++ components/solarRoof/control.js
@@ -75,7 +75,7 @@ const Control = () => {
                         <motion.p variants={variant1} initial='hidden' animate={Controls}
                          className="font-thin text-xl">Control</motion.p>
                         <motion.p variants={variant2} initial='hidden' animate={Controls}
-                         className="text-2xl md:text-3xl  lg:mb-3">Monitor and Optimize</motion.p>
+                         className="text-2xl md:text-3xl mb:2 lg:mb-3">Monitor and Optimize</motion.p>
                         <motion.button variants={variant4} initial='hidden' animate={Controls}
                          className="hidden sm:block  border-[3px]  mt-5 border-gray-800 h-8 rounded-full w-[200px] hover:text-white hover:bg-gray-800">
                             ORDER NOW

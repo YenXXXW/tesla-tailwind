6d2fe0d33cefb4f47e88a6008e78f073604d6499
YenXXXW <YenXXXW@gmai.com>
2022-09-04 16:13:37 +0630

    chech error

--- components/solarRoof/efficiency.js
+++ components/solarRoof/efficiency.js
@@ -79,7 +79,7 @@ const Efficiency = () => {
                 <div className="min-w-[250px] sm:my-auto lg:my-0">
                     <motion.p  variants={variant1} initial='hidden' animate={Controls}>Efficiency</motion.p>
                     <motion.p  variants={variant2} initial='hidden' animate={Controls}
-                    className="text-xl md:text-3xl font-bold lg:mb-3">Maximum Solar Production</motion.p>
+                    className="text-2xl md:text-3xl font-bold lg:mb-3">Maximum Solar Production</motion.p>
                     <motion.button  variants={variant4} initial='hidden' animate={Controls}
                      className="hidden sm:block lg:hidden border-[3px]  mt-5 border-gray-800 h-8 rounded-full w-[200px] hover:text-white hover:bg-gray-800">
                         ORDER NOW
@@ -89,7 +89,7 @@ const Efficiency = () => {
                     <motion.p  variants={variant3} initial='hidden' animate={Controls}
                     className="text-sm ">Generate the most energy possible, even on roofs with complicated angles and intermittent sunlight. Glass solar tiles and architectural-grade steel tiles, vent covers and ridge caps come together to form a roof that is both durable and powerful. With Tesla Solar Inverter, your fully integrated system is safe, reliable and outage ready. And built-in connectivity ensures Tesla Solar Inverter continues to improve with each over-the-air software update.</motion.p>
                     <motion.button variants={variant4} initial='hidden' animate={Controls}
-                     className="sm:hidden lg:block lg:mt-[180px] border-[3px]  mt-3 border-gray-800 h-8 rounded-full w-[200px] hover:text-white hover:bg-gray-800">
+                     className="sm:hidden lg:block lg:mt-[180px] border-[3px]  mt-6 border-gray-800 h-8 rounded-full w-[200px] hover:text-white hover:bg-gray-800">
                         ORDER NOW
                     </motion.button>
                 </div>

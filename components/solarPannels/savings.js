67e5f3deda78a49b2e793f1b978356c61a4c9a43
YenXXXW <YenXXXW@gmai.com>
2022-09-04 11:38:48 +0630

    check error

--- components/solarPannels/savings.js
+++ components/solarPannels/savings.js
@@ -67,29 +67,10 @@ const Savings = () => {
     
     return (
         <div className="w-full h-screen flex flex-col lg:flex-row-reverse pb-10">
-            <div className="w-full lg:w-[70%] relative" ref={ref}>
-                <video src='/SolarPannel/SPv1.mp4'  autoPlay loop defaultmuted='true' width='850px'/>
+            <div className="w-full lg:w-[70%] " ref={ref}>
+                <video src='/SolarPannel/SPv1.mp4'  autoPlay loop defaultmuted='true' width='940px'/>
             </div>
-            <div className="ml-8 flex flex-col sm:flex-row lg:flex-col lg:w-[29%] pt-3  lg:h-screen">
-                <div className="min-w-[250px] sm:my-auto lg:my-0">
-                    <motion.p  variants={variant1} initial='hidden' animate={Controls}>Savings</motion.p>
-                    <motion.p  variants={variant2} initial='hidden' animate={Controls}
-                    className="text-xl md:text-3xl font-bold lg:mb-3">Electricity For Less</motion.p>
-                    <motion.button  variants={variant4} initial='hidden' animate={Controls}
-                     className="hidden sm:block lg:hidden border-[3px]  mt-5 border-gray-800 h-8 rounded-full w-[200px] hover:text-white hover:bg-gray-800">
-                        See your Savings
-                    </motion.button>
-                </div>
-                <div className="sm:my-auto lg:my-0 pr-6">
-                    <motion.p  variants={variant3} initial='hidden' animate={Controls}
-                    className="text-sm ">Use solar energy to power your home and reduce your dependence on the grid. Purchase solar at the lowest price of any national provider with Tesla&apos;s price match guarantee and take control of your monthly electricity bill. Learn more about your potential savings in our Design Studio.</motion.p>
-                    <motion.button variants={variant4} initial='hidden' animate={Controls}
-                     className="sm:hidden lg:block lg:mt-[280px] border-[3px]  mt-3 border-gray-800 h-8 rounded-full w-[200px] hover:text-white hover:bg-gray-800">
-                        See your Savings
-                    </motion.button>
-                </div>
-                
-            </div>
+            
         </div>
 
     );

054db8548b239559e498c862ed1ba2e54bf1cbf9
YenXXXW <YenXXXW@gmai.com>
2022-09-04 17:56:08 +0630

    check SP powerWall

--- components/solarPannels/powerWall.js
+++ components/solarPannels/powerWall.js
@@ -73,9 +73,9 @@ const PowerWall = () => {
                 <div className="mx-auto  flex flex-col sm:flex-row max-w-[900px] pt-3 h-[50vh] md:h-[40vh] lg:h-[30vh] ">
                     <div className="lg:min-w-[350px] sm:my-auto mr-20" ref={ref}>
                         <motion.p variants={variant1} initial='hidden' animate={Controls}
-                         className="font-thin">Powerwall</motion.p>
+                          className="font-thin text-xl">Powerwall</motion.p>
                         <motion.p variants={variant2} initial='hidden' animate={Controls}
-                         className="text-xl md:text-3xl font-bold lg:mb-3">Home Battery Backup</motion.p>
+                         className="text-2xl md:text-3xl mb-3">Home Battery Backup</motion.p>
                         <motion.button variants={variant4} initial='hidden' animate={Controls}
                          className="hidden sm:block  border-[3px]  mt-5 border-gray-800 h-8 rounded-full w-[200px] hover:text-white hover:bg-gray-800">
                             ORDER NOW
@@ -83,9 +83,9 @@ const PowerWall = () => {
                     </div>
                     <div className="sm:my-auto pr-6">
                         <motion.p variants={variant3} initial='hidden' animate={Controls}
-                         className="text-sm ">Your Powerwall can be bundled with solar, allowing you to generate clean energy and store it for use anytime—at night or during an outage.</motion.p>
+                         className="sm:text-sm ">Your Powerwall can be bundled with solar, allowing you to generate clean energy and store it for use anytime—at night or during an outage.</motion.p>
                         <motion.button variants={variant4} initial='hidden' animate={Controls}
-                         className="sm:hidden   border-[3px]  mt-3 border-gray-800 h-8 rounded-full w-[200px] hover:text-white hover:bg-gray-800">
+                         className="sm:hidden   border-[3px]  mt-5 border-gray-800 h-8 rounded-full w-[200px] hover:text-white hover:bg-gray-800">
                             ORDER NOW
                         </motion.button>
                     </div>                    

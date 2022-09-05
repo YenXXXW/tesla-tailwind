e3a57a28413d2954e7a7debf7afa4e76798e1896
YenXXXW <YenXXXW@gmai.com>
2022-09-05 16:22:30 +0630

    check

--- components/modelX/last.js
+++ components/modelX/last.js
@@ -78,11 +78,11 @@ const Last =() =>{
         <div className="pt-10 bg-white">
         <div className="pt-10 ml-9 text-center md:flex lg:text-start bg-white">
             <div className="mx-auto lg:w-2/5" ref={TopicRef}>
-                <motion.p className="text-start xs:text-center text-2xl font-bold mt-7 mb-4 "
+                <motion.p className="text-center text-2xl font-bold mt-7 mb-4 "
                  variants={variant1} initial='hidden' animate={TopicControls}>
                     Model X
                 </motion.p>
-                <div className="flex flex-col lg:flex-row ">
+                <div className="flex justify-center flex-col lg:flex-row ">
                     <motion.button  variants={variant2} initial='hidden' animate={TopicControls}
                      className=" w-5/6 rounded-full text-lg font-bold border-[4px] border-gray-800 hover:bg-gray-800 hover:text-white hover:font-normal md:w-[250px] lg:w-[250px] lg:mr-20"
                     >
@@ -105,7 +105,7 @@ const Last =() =>{
         </div>
         <div className="mt-10 max-w-3xl text-gray-600 text-sm text-center mx-auto">
             Certain high data usage vehicle features require at least Standard Connectivity, including maps, navigation and voice commands. Access to features that use cellular data and third-party licenses are subject to change.<Link href="/"><a> Learn more about Standard Connectivity and any limitations.</a></Link>
-            <div className="my-20 flex flex-col md:flex-row justify-between">
+            <div className="py-10 flex flex-col md:flex-row justify-between">
             <Link href={'/'}><a className="no-underline">Tesla © 2022</a></Link>
             <Link href={'/'}><a className="no-underline">Privacy & Legal</a></Link>
             <Link href={'/'}><a className="no-underline">Vehicle Recalls</a></Link>
